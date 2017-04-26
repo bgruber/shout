f69e7ac2a861f8d367f4c32884c87505510a93b0
rxb <boenigk@gmail.com>
2017-04-26 16:52:53 -0400

    dummy chant

--- client/src/App.js
+++ client/src/App.js
@@ -7,15 +7,51 @@ import './App.css';
 window.timesync = timesync
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.incrementCursor = this.incrementCursor.bind(this);
+    this.startCursor = this.startCursor.bind(this);
+    this.state = {
+      cursor: 0,
+      chant: [
+        "no",
+        "ban",
+        "you",
+        "suck"
+      ]
+    };
+  }
+
+  incrementCursor() {
+    this.setState({
+      cursor: (this.state.cursor + 1)
+    });
+  }
+
+  startCursor(){
+    // interval start
+    this.setState({
+      interval: setInterval( this.incrementCursor, 1000)
+    });
+  }
+
+  componentDidMount() {
+    // init timesync?
+
+    // use timeout to decide when to call startcursor?
+    this.startCursor();
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to Shout</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        <p className="App-intro" style={{fontSize: 72, textAlign: 'center'}}>
+          {this.state.chant[this.state.cursor%this.state.chant.length]} <br />
+          {this.state.cursor}
         </p>
       </div>
     );
